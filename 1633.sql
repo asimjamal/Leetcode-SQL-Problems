11ec4abebf2696405f0e56941acc14531f2bb362
Asim Jamal <jamalasim081@gmail.com>
2025-09-14 21:32:07 -0400

    Add files via upload

---
+++ 1633.sql
@@ -0,0 +1,38 @@
+1633. Percentage of Users Attended a Contest
+Table: Users
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| user_id     | int     |
+| user_name   | varchar |
++-------------+---------+
+user_id is the primary key (column with unique values) for this table.
+Each row of this table contains the name and the id of a user.
+ 
+
+Table: Register
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| contest_id  | int     |
+| user_id     | int     |
++-------------+---------+
+(contest_id, user_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id of a user and the contest they registered into.
+ 
+
+Write a solution to find the percentage of the users registered in each contest rounded to two decimals.
+
+Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
+
+Solution:
+# Write your MySQL query statement below
+SELECT r.contest_id , 
+ROUND((COUNT(DISTINCT(r.USER_ID))*100/COUNT(DISTINCT(u.USER_ID))),2) as percentage
+FROM
+REGISTER r, 
+USERS u
+group by contest_id
+order by percentage desc ,contest_id asc;
